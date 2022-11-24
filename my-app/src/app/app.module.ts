04e56735ebd96fa993bb06b558c19172ea7c7dee
OlegSmol <118843738+OlegSmol@users.noreply.github.com>
2022-11-24 18:26:57 +0000

    practice 1

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyAppComponent } from './my-app/my-app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyAppComponent
   ],
   imports: [
     BrowserModule,
